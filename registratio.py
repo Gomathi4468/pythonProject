fa83c9f3af78155c6c93cf31626d99ecc7620f1a
Gomathi4468 <71780516+Gomathi4468@users.noreply.github.com>
2020-09-30 10:48:27 +0530

    Update registratio.py

--- registratio.py
+++ registratio.py
@@ -13,18 +13,18 @@ password.grid(row=1,column=1)
 def reg():
     uname=username.get()
     pwd=password.get()
-    f=open("admin.txt","a")
-    #for i in f:
-        #if (i.split(" ")[0]==uname):
-            #messagebox.showinfo("Register","Already exist")
-            #break
-    #else:
-    f.write(username)
-    f.write(" ")
-    f.write(password)
-    f.write(" ")
-    f.write("\n")
-    messagebox.showinfo("Register","Registration succesfully")
+    f=open("admin.txt","r+")
+    for i in f:
+        if (i.split(" ")[0]==uname):
+            messagebox.showinfo("Register","Already exist")
+            break
+    else:
+        f.write(username)
+        f.write(" ")
+        f.write(password)
+        f.write(" ")
+        f.write("\n")
+        messagebox.showinfo("Register","Registration succesfully")
     f.close()
-Button(text="Register", command="reg",font=('gothic',20)).grid(row=2, column=0,columnspan=2)
+Button(text="Register", command=reg,font=('gothic',20)).grid(row=2, column=0,columnspan=2)
 w.mainloop()
